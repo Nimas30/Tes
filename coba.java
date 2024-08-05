41973b856653e733215d254cbc64389dd835ebec
Nimas30 <nimas_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:55:17 +0700

    Add files via upload

---
+++ coba.java
@@ -0,0 +1,6 @@
+public class coba {
+    public static void main(String[] args) {
+        System.out.println("Perkenalkan nama saya Nimas Putu Abimanyu dari kelas X Rpl 7 yang berasal dari Malang");
+        String a = "1";    
+    }
+}
