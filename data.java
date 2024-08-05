250b31a960123b0f67d4c48b09ad002a388fc089
Nimas30 <nimas_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:05 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main (String[] args) {
+        //tipe_data nama_var  = value/isi var;
+        String nama = "Nimas";
+        int usia = 15;
+        String alamat = "Araya blok p5 no 19";
+        double kelas = 10.7;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Usia : " + usia + " tahun ");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
