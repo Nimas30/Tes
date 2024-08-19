6e5c4bc4b5ec9f03f84cba1f8e6557d60aa395ad
Nimas30 <nimas_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 13:58:00 +0700

    Add files via upload

---
+++ KaryawanTester.java
@@ -0,0 +1,20 @@
+//Driver Class
+public class KaryawanTester {
+    public static void main(String[] args) {
+        //object
+       //Class object = new Constructor
+       Karyawan Tia = new Karyawan ( 123, "Tia", "TU", "Malang");
+       Karyawan Alex = new Karyawan ( 87, "Alex", "Kurikulum", "Surabaya");
+       Karyawan Rudi = new Karyawan ( 9, "Rudi", "Kesiswaan", "Jember");
+       Karyawan Suna = new Karyawan ( 7, "Suna", "BK", "Mojokerto");
+       Karyawan Yasmin = new Karyawan ( 9, "Yasmin", "Sapras", "Sidoarjo");
+
+       Tia.print();
+       Alex.print();
+       Rudi.print();
+       Suna.print();
+       Yasmin.print();
+      
+
+    }
+}
