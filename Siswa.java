b6087b54d7ce42d35d050db3240c68c9f704b150
Nimas30 <nimas_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:45:26 +0700

    Add files via upload

--- Siswa.java
+++ Siswa.java
@@ -4,11 +4,51 @@ public class Siswa {
     String nama;
     double nilai;
 
-    //Constructor
-    //nama harus sama dgn Class
+    public int getAbsen() {
+        return this.absen;
+    }
+
+    public void setAbsen(int absen) {
+        this.absen = absen;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getNilai() {
+        return this.nilai;
+    }
+
+    public void setNilai(double nilai) {
+        this.nilai = nilai;
+    }
+
+    // Constructor
+    // nama harus sama dgn Class
     public Siswa() {
         absen = 0;
         nama = "kosong";
         nilai = 0.0;
     }
+
+    // Constructor Parameter
+    public Siswa(int absen, String nama, double nilai) {
+        // variabel = parameter
+        this.absen = absen;
+        this.nama = nama;
+        this.nilai = nilai;
+    }
+
+    // method print
+    public void print() {
+        System.out.println("Absen: " + absen);
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+    }
+
 }
